fd4583fd41c9fdd2ba96dc5269dc491154c3d388
Abdel-Khafid kolawole Fifa SALAOU <119133987+adeola003@users.noreply.github.com>
2023-05-09 21:46:45 +0100

    create nav components

--- src/store.js
+++ src/store.js
@@ -1,1 +1,8 @@
-import
+import { configureStore } from "@reduxjs/toolkit";
+import cartReducer from "./features/cart/cartSlice";
+
+export const store = configureStore({
+    reducer: {
+        cart: cartReducer
+    },
+});
